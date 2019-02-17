08c0de0579124e66e36931e7a61bd4713a34859b
Joel Collins <joel@jtcollins.net>
2019-02-17 11:43:52 +0000

    Added microscope settings placeholder

--- src/components/paneSettings.vue
+++ src/components/paneSettings.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="paneSettings">
-    <h3>Settings</h3>
+    
+    <ul uk-accordion="multiple: true">
+      <li>
+        <a class="uk-accordion-title" href="#">App settings</a>
+        <div class="uk-accordion-content">
 
-    <div class="uk-child-width-1-2" uk-grid>
-      <p><label><input v-model="disableStream" class="uk-checkbox" type="checkbox"> Disable stream</label></p>
-      <p><label class="uk-disabled"><input v-model="autoGpuPreview" class="uk-checkbox" type="checkbox"> GPU preview</label></p>
-    </div>
+          <div class="uk-child-width-1-2" uk-grid>
+            <p><label><input v-model="disableStream" class="uk-checkbox" type="checkbox"> Disable stream</label></p>
+            <p><label class="uk-disabled"><input v-model="autoGpuPreview" class="uk-checkbox" type="checkbox"> GPU preview</label></p>
+          </div>
+        </div>
+      </li>
+
+      <li>
+        <a class="uk-accordion-title" href="#">Microscope settings</a>
+        <div class="uk-accordion-content">
+          <div class="uk-placeholder uk-text-center">Microscope config coming soon...</div>
+      </li>
+
+    </ul>
 
   </div>
 </template>
