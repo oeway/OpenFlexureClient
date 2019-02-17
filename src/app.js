1e837c594fd29cd3c846c8f42d04b67f55a6a301
Joel Collins <joel@jtcollins.net>
2019-02-17 11:43:40 +0000

    Added context menu

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const contextMenu = require('electron-context-menu');
+
+const isDev = process.env.NODE_ENV === 'DEV';
+var isWin = process.platform === "win32";
+
 
 let url
 console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === 'DEV') {
+if (isDev) {
   console.log("Loading from localhost")
   url = 'http://localhost:8080/'
 } else {
@@ -14,5 +19,13 @@ if (process.env.NODE_ENV === 'DEV') {
 
 app.on('ready', () => {
   let window = new BrowserWindow({width: 1124, height: 800})
+  window.setMenu(null);
+
+  contextMenu({
+    showCopyImageAddress: true,
+    showSaveImageAs: true,
+    showInspectElement: isDev,
+  });
+
   window.loadURL(url)
 })
