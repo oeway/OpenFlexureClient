844d0109d174f87bf1d91437b8653acadbf53fc4
Joel Collins <joel@jtcollins.net>
2019-02-19 17:18:05 +0000

    Moved app settings to panel

---
+++ src/components/paneSettingsComponents/appSettings.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="appSettings">
+
+  <div class="uk-child-width-1-2" uk-grid>
+    <p><label><input v-model="disableStream" class="uk-checkbox" type="checkbox"> Disable stream</label></p>
+    <p><label class="uk-disabled"><input v-model="autoGpuPreview" class="uk-checkbox" type="checkbox"> GPU preview</label></p>
+  </div>
+
+</div>
+</template>
+
+<script>
+
+// Export main app
+export default {
+  name: 'appSettings',
+
+  computed: {
+    disableStream: {
+      get() {
+        return this.$store.state.settings.disableStream;
+      },
+      set(value) {
+        this.$store.commit("changeSetting", ['disableStream', value]);
+      }
+    },
+    autoGpuPreview: {
+      get() {
+        return this.$store.state.settings.autoGpuPreview;
+      },
+      set(value) {
+        this.$store.commit("changeSetting", ['autoGpuPreview', value]);
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+</style>
