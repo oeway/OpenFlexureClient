4b57149a93adbbedf480678bc2a28b3b49e351d2
Joel Collins <joel@jtcollins.net>
2019-02-28 18:33:18 +0000

    Dynamically generate appx manifest

--- installers/create-appx.js
+++ installers/create-appx.js
@@ -1,16 +1,22 @@
 const convertToWindowsStore = require('electron-windows-store')
 const path = require('path')
+const package = require('../package.json');
 
 convertToWindowsStore({
    containerVirtualization: false,
    inputDirectory: path.join(__dirname, '../release-builds/openflexure-microscope-js-win32-x64'),
    outputDirectory: path.join(__dirname, '../release-builds/installers'),
    assets: path.join(__dirname, '/appx/assets'),
-   manifest: path.join(__dirname, '/appx/AppXManifest.xml'),
+   packageExecutable: 'app/openflexure-microscope-js.exe',
    packageName: 'OpenFlexureMicroscopeJS',
+   packageVersion: `${package.version}.0`,
+   packageDisplayName: package.productName,
+   packageDescription: package.description,
+   packageBackgroundColor: '#9C1561',
+   publisherDisplayName: package.publisher,
    deploy: false,
    publisher: 'CN=bath-open-instrumentation-group',
    publisherDisplayName: "Bath Open Instrumentation Group",
-   devCert: "C:\\Users\\jtc92\\AppData\\Roaming\\electron-windows-store\\bath-open-instrumentation-group\\bath-open-instrumentation-group.pfx",
+   devCert: "C:\\Certs\\openflexure-microscope-js-appx\\openflexure-microscope-js-appx.pfx",
    windowsKit: 'C:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.17763.0\\x64'
 })
