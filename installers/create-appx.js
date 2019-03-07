db4b9bf290fc9dc194aa48989b062ca69049f471
Joel Collins <joel@jtcollins.net>
2019-03-07 21:21:05 +0000

    Turn dev versions into minor appx versions

--- installers/create-appx.js
+++ installers/create-appx.js
@@ -2,6 +2,19 @@ const convertToWindowsStore = require('electron-windows-store')
 const path = require('path')
 const package = require('../package.json');
 
+var majorVer = package.version
+var minorVer = 0
+
+if (majorVer.indexOf("alpha")  > -1) {
+   minorVer = 1
+}
+else if (majorVer.indexOf("beta")  > -1) {
+   minorVer = 5
+}
+
+var numericVersion = package.version.replace(/[^\d.]/g, '')
+var appxVersion = `${numericVersion}.${minorVer}`
+
 convertToWindowsStore({
    containerVirtualization: false,
    inputDirectory: path.join(__dirname, '../release-builds/openflexure-ev-win32-x64'),
@@ -10,7 +23,7 @@ convertToWindowsStore({
    identityName: '60425J.T.Collins.OpenFlexureeV',
    packageExecutable: 'app/openflexure-ev.exe',
    packageName: 'OpenFlexureEV',
-   packageVersion: `${package.version}.0`,
+   packageVersion: appxVersion,
    packageDisplayName: package.productName,
    packageDescription: package.description,
    packageBackgroundColor: '#9C1561',
