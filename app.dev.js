6227e2de581a5f697ad2ba5c9068d39148ed4654
Joel Collins <joel@jtcollins.net>
2019-03-21 23:20:35 +0000

    Added menubar back to dev electron app

--- app.dev.js
+++ app.dev.js
@@ -6,8 +6,15 @@ const contextMenu = require('electron-context-menu');
 let url = 'http://localhost:8080/'
 
 app.on('ready', () => {
-  let window = new BrowserWindow({width: 1124, height: 800})
-  window.setMenu(null);
+  let window = new BrowserWindow({
+    width: 1124, 
+    height: 800,
+    // Remove the window frame from windows applications
+    frame: false,
+    // Hide the titlebar from MacOS applications while keeping the stop lights
+    titleBarStyle: 'hidden', // or 'customButtonsOnHover',
+  })
+  //window.setMenu(null);
 
   contextMenu({
     showCopyImageAddress: true,
