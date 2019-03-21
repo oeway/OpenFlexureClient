7fd0791108c7456c578234d7e3d34f43de2d4360
Joel Collins <joel@jtcollins.net>
2019-03-21 16:26:07 +0000

    Added mixin for preview-hiding modal confirmations

--- src/main.js
+++ src/main.js
@@ -2,7 +2,35 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import UIkit from 'uikit';
+
 Vue.config.productionTip = false
+
+Vue.mixin({
+  methods: {
+    modalConfirm: function(modalText) {
+      var self = this;
+
+      // Stop GPU preview to show modal
+      self.$root.$emit('globalTogglePreview', false)
+  
+      var showModal = function(resolve, reject) {
+        UIkit.modal.confirm(modalText)
+        .then(function() {
+          resolve()
+        }, function () {
+          reject()
+        })
+        .finally(function() {
+          // Reenable the GPU preview, if it was active before the modal
+          console.log("Re-enabling GPU preview")
+          self.$root.$emit('globalReenablePreview')
+        })
+      }
+      return new Promise(showModal)
+    }
+  }
+})
 
 new Vue({
   store,
