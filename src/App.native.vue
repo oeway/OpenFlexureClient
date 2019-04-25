97cc5d2b787993876c05c2ee06f1065daa87cc6f
Joel Collins <joel@jtcollins.net>
2019-04-25 14:35:52 +0100

    Implemented NativeScript-Vue CLI Plugin

---
+++ src/App.native.vue
@@ -0,0 +1,38 @@
+<template>
+  <Page>
+    <ActionBar title="OpenFlexure Native Demo"/>
+    <GridLayout rows="auto, auto">
+      <Label class="message" text="No content currently ported" row="0" horizontalAlignment="center"/>
+    </GridLayout>
+  </Page>
+</template>
+
+<script>
+// Import axios for HTTP requests
+import axios from 'axios'
+
+// Export main app
+export default {
+  name: 'app',
+
+
+  data: function () {
+    return {}  
+  },
+
+  created: function () {
+  },
+
+  beforeDestroy: function () {
+  },
+
+  methods: {
+  },
+
+  computed: {
+  }
+
+}
+</script>
+
+<style></style>
