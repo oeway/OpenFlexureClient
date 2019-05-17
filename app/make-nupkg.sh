dbc9e3947316c6c787a8ad129575ad90d7fd5fa8
Joel Collins <joel@jtcollins.net>
2019-05-17 16:11:58 +0100

    Updated iconUrl to static file

--- app/make-nupkg.sh
+++ app/make-nupkg.sh
@@ -69,7 +69,7 @@ cat > "$outpath/openflexure-ev.nuspec" <<- EOL
     <tags>openflexure microscope ev</tags>
     <licenseUrl>https://gitlab.com/openflexure/openflexure-microscope-jsclient/raw/master/LICENSE</licenseUrl>
     <requireLicenseAcceptance>false</requireLicenseAcceptance>
-    <iconUrl>https://gitlab.com/openflexure/openflexure-microscope-jsclient/raw/master/app/icons/png/512x512.png</iconUrl>
+    <iconUrl>https://build.openflexure.org/openflexure_ev/512x512.png</iconUrl>
   </metadata>
   <files>
     <file src="tools/**" target="tools" />
