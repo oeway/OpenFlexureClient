396adca35f1022bd30fd9639e6ccedfa393e7146
Joel Collins <joel@jtcollins.net>
2019-06-02 21:15:32 +0100

    Separate app and web builds

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  configureWebpack: config => {
+    config.target = process.env.VUE_APP_TARGET || 'web',
+    config.entry = (process.env.VUE_APP_TARGET == 'electron-renderer') 
+    ? {
+      main: './src/main.js',
+      app: './src/main.app.js'
+    }
+    :{
+      main: './src/main.js',
+    }
+  },
+  outputDir: (process.env.VUE_APP_TARGET == 'electron-renderer') ? './app/dist' : './dist',
   publicPath: ''
 }
