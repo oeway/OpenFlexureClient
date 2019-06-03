22d139ecf88db80658daea1401597a4b9e1b399f
Joel Collins <joel@jtcollins.net>
2019-06-03 13:38:03 +0100

    Added titlebar icon

--- src/main.app.js
+++ src/main.app.js
@@ -1,9 +1,11 @@
 import { Titlebar, Color } from 'custom-electron-titlebar'
 
+
 // Only show custom menubar for Windows or Linux
 if ((process.platform !== 'darwin')) {
     new Titlebar({
-        backgroundColor: Color.fromHex('#c5247f')
+        backgroundColor: Color.fromHex('#c5247f'),
+        icon: './titleicon.svg'
     });
 }
 
